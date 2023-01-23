b30047e7203b93af6e4622d0047c1e09fcdaf8bf
MarkVuyisile <116636579+MarkVuyisile@users.noreply.github.com>
2023-01-23 13:51:09 +0200

    Please view my comments here.
    I think you should normalise naming routes so that the next person is able to see the intention of the action created.

--- backend/routes.js
+++ backend/routes.js
@@ -6,7 +6,14 @@ const {
   putContact,
   deleteContact,
 } = require("./controllers");
-
+/**
+*Mark: I think you should start practicing route naming.
+*e.g:
+*get should be something like "/get/contacts"
+*post should be something like "/create/contact"
+*put should be something like "/update/contact"
+*delete should be something like "/delete/contact"
+*/
 router.get("/", getContacts);
 router.post("/", setContact);
 
